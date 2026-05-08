02132aecd7c519580cb7c5a8db07aeac66716f96
Oleg Zamesov <o.zamesov@yandex.ru>
2026-05-08 11:23:47 +0300

    add log

--- django_event_bus/management/commands/run_event_consumer.py
+++ django_event_bus/management/commands/run_event_consumer.py
@@ -1,10 +1,15 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
 from django_event_bus.consumer import start_consumer
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Run event bus consumer'
 
     def handle(self, *args, **options):
+        print('Running event bus consumer')
+        logger.info('running event bus consumer')
         start_consumer()
