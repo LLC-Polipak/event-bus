b013703f1c9cf62f48debb998b3efe2bc8aa7392
Oleg Zamesov <o.zamesov@yandex.ru>
2026-08-21 15:38:18 +0300

    fix import

--- example/users/handlers/__init__.py
+++ example/users/handlers/__init__.py
@@ -1,5 +1,5 @@
 """Публичные обработчики событий демонстрационного users-приложения."""
 
-from example.users.handlers.handler import handle_example
+from users.handlers.handler import handle_example
 
 __all__ = ['handle_example']
